ae362edca14c7991ce90fd7a2de917af8902c109
ptrvsrg <s.petrov1@g.nsu.ru>
2023-05-03 02:03:04 +0700

    Add connection classes

---
+++ src/main/java/ru/nsu/ccfit/petrov/task5/connection/Connection.java
@@ -0,0 +1,82 @@
+package ru.nsu.ccfit.petrov.task5.connection;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.UUID;
+import lombok.Getter;
+import ru.nsu.ccfit.petrov.task5.listener.Listener;
+import ru.nsu.ccfit.petrov.task5.listener.ListeningSupport;
+import ru.nsu.ccfit.petrov.task5.connection.event.DisconnectEvent;
+import ru.nsu.ccfit.petrov.task5.connection.event.MessageReceivedEvent;
+import ru.nsu.ccfit.petrov.task5.message.Message;
+
+/**
+ * The type {@code Connection} is class that contains client socket, sends and receives messages. {@code Connection}
+ * Thread constantly receives messages from the socket and sends events
+ */
+public abstract class Connection
+    extends Thread {
+
+    @Getter private final UUID connectionId = UUID.randomUUID();
+    private final Socket clientSocket;
+    /**
+     * The client socket input stream.
+     */
+    protected final ObjectInputStream in;
+    /**
+     * The client socket output stream.
+     */
+    protected final ObjectOutputStream out;
+    private final ListeningSupport listeningSupport = new ListeningSupport();
+
+    /**
+     * Instantiates a new Connection.
+     *
+     * @param clientSocket the client socket
+     * @throws IOException if an I/O error occurs when creating the input stream, the socket is closed, the socket is
+     *                     not connected, or the socket input has been shutdown using
+     *                     {@link java.net.Socket#shutdownInput()}
+     */
+    protected Connection(Socket clientSocket)
+        throws IOException {
+        this.clientSocket = clientSocket;
+        this.in = new ObjectInputStream(clientSocket.getInputStream());
+        this.out = new ObjectOutputStream(clientSocket.getOutputStream());
+    }
+
+    /**
+     * Sends message over socket.
+     *
+     * @param message the message
+     * @return {@code true} - message is sent, {@code false} - sending error is happened or message format is invalid
+     */
+    public abstract boolean send(Message message);
+
+    /**
+     * Receives message through socket.
+     *
+     * @return the message, or {@code null} - receiving error is happened, or message format is invalid
+     */
+    public abstract Message receive();
+
+    @Override
+    public void run() {
+        while (!clientSocket.isClosed() && clientSocket.isConnected()) {
+            Message message = receive();
+            listeningSupport.notifyListeners(new MessageReceivedEvent(connectionId, message));
+        }
+
+        listeningSupport.notifyListeners(new DisconnectEvent(connectionId));
+    }
+
+    /**
+     * Add listener.
+     *
+     * @param listener the listener
+     */
+    public void addListener(Listener listener) {
+        listeningSupport.addListener(listener);
+    }
+}
---
+++ src/main/java/ru/nsu/ccfit/petrov/task5/connection/object/JavaObjectConnection.java
@@ -0,0 +1,63 @@
+package ru.nsu.ccfit.petrov.task5.connection.object;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Objects;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Level;
+import ru.nsu.ccfit.petrov.task5.connection.Connection;
+import ru.nsu.ccfit.petrov.task5.message.Message;
+import ru.nsu.ccfit.petrov.task5.message.object.JavaObjectMessage;
+
+/**
+ * The type {@code JavaObjectConnection} is class that implements abstract method of {@link Connection} for sending and
+ * receiving Java objects.
+ */
+@Log4j2
+public class JavaObjectConnection
+    extends Connection {
+
+    /**
+     * Instantiates a new JavaObjectConnection.
+     *
+     * @param socket the socket
+     * @throws IOException if an I/O error occurs when creating the input stream, the socket is closed, the socket is
+     *                     not connected, or the socket input has been shutdown using
+     *                     {@link java.net.Socket#shutdownInput()}
+     */
+    public JavaObjectConnection(Socket socket)
+        throws IOException {
+        super(socket);
+    }
+
+    @Override
+    public boolean send(Message message) {
+        if (!Objects.equals(message.getClass(), JavaObjectMessage.class)) {
+            log.error("Invalid message format");
+            return false;
+        }
+
+        try {
+            out.writeObject(message);
+        } catch (IOException e) {
+            log.catching(Level.ERROR, e);
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public Message receive() {
+        JavaObjectMessage message = null;
+        try {
+            message = (JavaObjectMessage) in.readObject();
+        } catch (ClassNotFoundException | IOException e) {
+            log.catching(Level.ERROR, e);
+        } catch (ClassCastException e) {
+            log.error("Invalid message format");
+        }
+
+        return message;
+    }
+}
