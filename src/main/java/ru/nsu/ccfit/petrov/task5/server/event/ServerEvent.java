fe524a41fdf163d5a9d86d6588de1a202ad396ec
ptrvsrg <s.petrov1@g.nsu.ru>
2023-05-03 18:20:30 +0700

    Make an interface

--- src/main/java/ru/nsu/ccfit/petrov/task5/server/event/ServerEvent.java
+++ src/main/java/ru/nsu/ccfit/petrov/task5/server/event/ServerEvent.java
@@ -1,7 +1,5 @@
 package ru.nsu.ccfit.petrov.task5.server.event;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import ru.nsu.ccfit.petrov.task5.listener.Event;
 
 /**
@@ -9,40 +7,5 @@ import ru.nsu.ccfit.petrov.task5.listener.Event;
  *
  * @author ptrvsrg
  */
-@Getter
-@RequiredArgsConstructor
-public abstract class ServerEvent
-    implements Event {
-
-    /**
-     * The type {@code ServerEventType} is enum that describes server event types.
-     */
-    public enum ServerEventType {
-        /**
-         * Login request received server event type.
-         */
-        LOGIN_REQUEST_RECEIVED,
-        /**
-         * New message request received server event type.
-         */
-        NEW_MESSAGE_REQUEST_RECEIVED,
-        /**
-         * User list request received server event type.
-         */
-        USER_LIST_REQUEST_RECEIVED,
-        /**
-         * Logout request received server event type.
-         */
-        LOGOUT_REQUEST_RECEIVED,
-        /**
-         * Session timeout server event type.
-         */
-        SESSION_TIMEOUT,
-        /**
-         * Timer finished server event type.
-         */
-        STOPWATCH_FINISHED
-    }
-
-    private final ServerEventType type;
-}
+public interface ServerEvent
+    extends Event {}
