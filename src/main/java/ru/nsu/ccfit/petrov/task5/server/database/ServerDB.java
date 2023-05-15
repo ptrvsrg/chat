08fdd61979230c19919cbf782430667bd54b0a45
ptrvsrg <s.petrov1@g.nsu.ru>
2023-05-15 14:33:15 +0700

    Add getUserByConnection and removeUser method

--- src/main/java/ru/nsu/ccfit/petrov/task5/server/database/ServerDB.java
+++ src/main/java/ru/nsu/ccfit/petrov/task5/server/database/ServerDB.java
@@ -1,5 +1,6 @@
 package ru.nsu.ccfit.petrov.task5.server.database;
 
+import java.util.Map.Entry;
 import lombok.Getter;
 import ru.nsu.ccfit.petrov.task5.connection.Connection;
 
@@ -14,8 +15,26 @@ public class ServerDB {
         users.put(userName, connection);
     }
 
+    public String getUserByConnection(Connection connection) {
+        for (Entry<String, Connection> entry : users.entrySet()) {
+            if (entry.getValue() == connection) {
+                return entry.getKey();
+            }
+        }
+
+        return null;
+    }
+
     public void removeUser(String userName) {
         users.remove(userName);
+    }
+
+    public void removeUser(Connection connection) {
+        for (Entry<String, Connection> entry : users.entrySet()) {
+            if (entry.getValue() == connection) {
+                users.remove(entry.getKey());
+            }
+        }
     }
 
 }
