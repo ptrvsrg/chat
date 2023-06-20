fecf38a88040c1e84a35054287aaa27106f25efe
ptrvsrg <s.petrov1@g.nsu.ru>
2023-06-20 12:13:42 +0700

    Get and check server parameters in one place

--- src/main/java/ru/nsu/ccfit/petrov/task5/server/ServerApp.java
+++ src/main/java/ru/nsu/ccfit/petrov/task5/server/ServerApp.java
@@ -1,9 +1,20 @@
 package ru.nsu.ccfit.petrov.task5.server;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import ru.nsu.ccfit.petrov.task5.dto.DTOFormat;
+import ru.nsu.ccfit.petrov.task5.server.config.ServerConfig;
+
 public class ServerApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+        throws IOException {
+        InetAddress address = ServerConfig.getAddress();
+        int port = ServerConfig.getPort();
+        int timeout = ServerConfig.getTimeout();
+        DTOFormat dtoFormat = ServerConfig.getDTOFormat();
+
         Server server = new Server();
-        server.start();
+        server.start(address, port, timeout, dtoFormat);
     }
 }
