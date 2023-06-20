1237786e326df20245eae732f4e76d37f17356e9
ptrvsrg <s.petrov1@g.nsu.ru>
2023-06-20 12:10:09 +0700

    Add lazy reading config file

--- src/main/java/ru/nsu/ccfit/petrov/task5/server/config/ServerConfig.java
+++ src/main/java/ru/nsu/ccfit/petrov/task5/server/config/ServerConfig.java
@@ -2,41 +2,61 @@ package ru.nsu.ccfit.petrov.task5.server.config;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetAddress;
 import java.util.Properties;
-import lombok.extern.log4j.Log4j2;
-import org.apache.logging.log4j.Level;
 import ru.nsu.ccfit.petrov.task5.dto.DTOFormat;
 
-@Log4j2
 public class ServerConfig {
 
     private static final String CONFIG_FILE_NAME = "server.properties";
-    private static final Properties properties = new Properties();
+    private static Properties properties;
 
     private ServerConfig() {
         throw new IllegalStateException("Utility class");
     }
 
-    static {
-        try (InputStream configFile = ServerConfig.class.getClassLoader().getResourceAsStream(CONFIG_FILE_NAME)) {
-            properties.load(configFile);
-        } catch (IOException e) {
-            log.catching(Level.FATAL, e);
-            throw new RuntimeException(e);
-        }
+    private static void readConfigFile()
+        throws IOException {
+        InputStream configFile = ServerConfig.class.getClassLoader()
+                                                   .getResourceAsStream(CONFIG_FILE_NAME);
+        properties = new Properties();
+        properties.load(configFile);
     }
 
-    public static int getPort() {
+    public static InetAddress getAddress()
+        throws IOException {
+        if (properties == null) {
+            readConfigFile();
+        }
+
+        return InetAddress.getByName(properties.getProperty("connection.address"));
+    }
+
+    public static int getPort()
+        throws IOException {
+        if (properties == null) {
+            readConfigFile();
+        }
+
         return Integer.parseInt(properties.getProperty("connection.port"));
     }
 
-    public static int getTimeout() {
+    public static int getTimeout()
+        throws IOException {
+        if (properties == null) {
+            readConfigFile();
+        }
+
         return Integer.parseInt(properties.getProperty("connection.timeout"));
     }
 
-    public static DTOFormat getDTOFormat() {
-        String dtoFormat = properties.getProperty("DTO.format");
+    public static DTOFormat getDTOFormat()
+        throws IOException {
+        if (properties == null) {
+            readConfigFile();
+        }
 
+        String dtoFormat = properties.getProperty("DTO.format");
         switch (dtoFormat) {
             case "java_object":
                 return DTOFormat.JAVA_OBJECT;
