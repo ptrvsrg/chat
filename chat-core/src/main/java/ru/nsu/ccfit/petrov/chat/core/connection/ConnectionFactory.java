54b220167a10f7bc5404266d8cafdcffd07815c8
ptrvsrg <s.petrov1@g.nsu.ru>
2023-06-25 12:26:11 +0700

    Make Connection interface

--- chat-core/src/main/java/ru/nsu/ccfit/petrov/chat/core/connection/ConnectionFactory.java
+++ chat-core/src/main/java/ru/nsu/ccfit/petrov/chat/core/connection/ConnectionFactory.java
@@ -1,6 +1,11 @@
 package ru.nsu.ccfit.petrov.chat.core.connection;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import ru.nsu.ccfit.petrov.chat.core.dto.DTOFormat;
 
@@ -9,16 +14,20 @@ public class ConnectionFactory {
     private ConnectionFactory() {
         throw new IllegalStateException("Utility class");
     }
-    
+
     public static Connection newConnection(DTOFormat dtoFormat, Socket clientSocket)
         throws IOException {
         switch (dtoFormat) {
             case JAVA_OBJECT:
-                return new JavaObjectConnection(clientSocket);
+                return new JavaObjectConnection(
+                    new ObjectInputStream(clientSocket.getInputStream()),
+                    new ObjectOutputStream(clientSocket.getOutputStream()));
             case XML_FILE:
-                return new XmlFileConnection(clientSocket);
+                return new XmlFileConnection(
+                    new BufferedReader(new InputStreamReader(clientSocket.getInputStream())),
+                    new PrintWriter(clientSocket.getOutputStream(), true));
             default:
-                throw new IllegalStateException("Unsupported connection format");
+                throw new IllegalArgumentException("Unsupported DTO format");
         }
     }
 }
