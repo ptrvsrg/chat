6856a4b0f1b3d4e53c121de4010571eaf941d406
ptrvsrg <s.petrov1@g.nsu.ru>
2023-06-26 21:14:08 +0700

    Implement connect, close; Make synchronized send and receive

--- chat-core/src/main/java/ru/nsu/ccfit/petrov/chat/core/connection/JavaObjectConnection.java
+++ chat-core/src/main/java/ru/nsu/ccfit/petrov/chat/core/connection/JavaObjectConnection.java
@@ -3,6 +3,7 @@ package ru.nsu.ccfit.petrov.chat.core.connection;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.Socket;
 import lombok.RequiredArgsConstructor;
 import ru.nsu.ccfit.petrov.chat.core.dto.DTO;
 
@@ -10,26 +11,53 @@ import ru.nsu.ccfit.petrov.chat.core.dto.DTO;
 public class JavaObjectConnection
     implements Connection {
 
-    private final ObjectInputStream in;
-    private final ObjectOutputStream out;
+    private Socket clientSocket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
 
     @Override
-    public void send(DTO dto)
+    public void connect(Socket clientSocket)
         throws IOException {
-        synchronized (out) {
-            out.writeObject(dto);
+        this.clientSocket = clientSocket;
+        this.in = new ObjectInputStream(clientSocket.getInputStream());
+        this.out = new ObjectOutputStream(clientSocket.getOutputStream());
+    }
+
+    @Override
+    public synchronized void send(DTO dto)
+        throws IOException {
+        if (out == null) {
+            throw new IOException("Connection not established");
+        }
+
+        out.writeObject(dto);
+    }
+
+    @Override
+    public synchronized DTO receive()
+        throws IOException {
+        if (in == null) {
+            throw new IOException("Connection not established");
+        }
+
+        try {
+            return (DTO) in.readObject();
+        } catch (ClassNotFoundException | ClassCastException e) {
+            throw new IOException("Invalid DTO format : " + e);
         }
     }
 
     @Override
-    public DTO receive()
+    public void close()
         throws IOException {
-        try {
-            synchronized (in) {
-                return (DTO) in.readObject();
-            }
-        } catch (ClassNotFoundException | ClassCastException e) {
-            throw new IOException("Invalid DTO format : " + e);
+        if (clientSocket != null) {
+            clientSocket.close();
+        }
+        if (in != null) {
+            in.close();
+        }
+        if (out != null) {
+            out.close();
         }
     }
 }
--- chat-core/src/main/java/ru/nsu/ccfit/petrov/chat/core/connection/XmlFileConnection.java
+++ chat-core/src/main/java/ru/nsu/ccfit/petrov/chat/core/connection/XmlFileConnection.java
@@ -2,7 +2,9 @@ package ru.nsu.ccfit.petrov.chat.core.connection;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.Socket;
 import javax.xml.bind.JAXBException;
 import lombok.RequiredArgsConstructor;
 import ru.nsu.ccfit.petrov.chat.core.dto.DTO;
@@ -12,12 +14,25 @@ import ru.nsu.ccfit.petrov.chat.core.dto.XmlUtils;
 public class XmlFileConnection
     implements Connection {
 
-    private final BufferedReader in;
-    private final PrintWriter out;
+    private Socket clientSocket;
+    private BufferedReader in;
+    private PrintWriter out;
 
     @Override
-    public void send(DTO dto)
+    public void connect(Socket clientSocket)
         throws IOException {
+        this.clientSocket = clientSocket;
+        this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        this.out = new PrintWriter(clientSocket.getOutputStream(), true);
+    }
+
+    @Override
+    public synchronized void send(DTO dto)
+        throws IOException {
+        if (out == null) {
+            throw new IOException("Connection not established");
+        }
+
         String xml;
         try {
             xml = XmlUtils.dtoToXml(dto);
@@ -25,23 +40,36 @@ public class XmlFileConnection
             throw new IOException("Invalid DTO format : " + e);
         }
 
-        synchronized (out) {
-            out.println(xml);
-        }
+        out.println(xml);
     }
 
     @Override
-    public DTO receive()
+    public synchronized DTO receive()
         throws IOException {
-        String xml;
-        synchronized (in) {
-            xml = in.readLine();
+        if (in == null) {
+            throw new IOException("Connection not established");
         }
+
+        String xml = in.readLine();
 
         try {
             return XmlUtils.xmlToDto(xml);
         } catch (JAXBException e) {
             throw new IOException("Invalid DTO format : " + e);
+        }
+    }
+
+    @Override
+    public void close()
+        throws IOException {
+        if (clientSocket != null) {
+            clientSocket.close();
+        }
+        if (in != null) {
+            in.close();
+        }
+        if (out != null) {
+            out.close();
         }
     }
 }
